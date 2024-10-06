f33e2fbe13907a6269290caacfd8682c97c7fce6
jeanheberth <jeanheberth19@gmail.com>
2024-10-06 12:00:53 -0300

    refatorando testes de deletar card.

--- cypress/integration/api/card/deletandoCard.cy.js
+++ cypress/integration/api/card/deletandoCard.cy.js
@@ -1,6 +1,6 @@
 describe('Deve deletar um card no Trello', () => {
     it('Deve deletar um card', () => {
-        cy.readFile('cypress/api/fixtures/card/dados.json').then((dados) => {
+        cy.readFile('cypress/fixtures/card.json').then((dados) => {
             cy.request({
                 method: 'DELETE',
                 url: `https://api.trello.com/1/cards/${dados.cardId}?key=${Cypress.env('apiKey')}&token=${Cypress.env('apiToken')}`,
